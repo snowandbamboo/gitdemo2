022652b0d98d7efb15562f0016d18f0ad93c3db1
摩卡 <1215134782@qq.com>
2019-07-07 22:20:48 +0800

    add

--- src/main/java/T.java
+++ src/main/java/T.java
@@ -3,5 +3,6 @@ public class T {
         System.out.println("hehe");
 	System.out.println(" yujianan shi yi ge hao ren");
         System.out.println("xiao bian nihao");
+        System.out.println("我是一个中国人");
     }
 }
