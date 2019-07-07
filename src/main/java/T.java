a3c5176c59b3fde1c085bbcd605a3aaed2093cb9
摩卡 <1215134782@qq.com>
2019-07-07 22:15:38 +0800

    add

--- src/main/java/T.java
+++ src/main/java/T.java
@@ -2,5 +2,6 @@ public class T {
     public static void main(String[] args) {
         System.out.println("hehe");
 	System.out.println(" yujianan shi yi ge hao ren");
+        System.out.println("xiao bian nihao");
     }
 }
